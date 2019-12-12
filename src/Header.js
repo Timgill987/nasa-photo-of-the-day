2f18dfc959116f3fe83b914a6327beb76db5268c
Timgill987 <56654688+Timgill987@users.noreply.github.com>
2019-12-11 20:16:15 -0500

    finished MVP

--- src/Header.js
+++ src/Header.js
@@ -1,42 +1,24 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import axios from "axios";
-import AppContent from "./Header";
-function App() {
-  const [appStuff, setAppStuff] = useState([]);
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.nasa.gov/planetary/apod?api_key=R2hhFJdU7TuiecabWphEVkjhUWYwHLUXUaML2bvu`
-      )
-      .then(response => {
-        console.log(response.data);
-        setAppStuff(response.data);
-      })
-      .catch(error => {
-        console.log("the data was not return", error);
-      });
-  }, []);
-  console.log(appStuff);
-  return (
-    <div className="App">
-      <AppContent
-        key={appStuff.id}
-        title={appStuff.title}
-        explanation={appStuff.explanation}
-        image={appStuff.url}
-        date={appStuff.date}
-      />
+import React from "react";
 
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+const AppContent = props => {
+    console.log(props)
+  return (
+    <div className="nasa-container" key={props.id}>
+      <div className="top-stuff">
+        <h1>{props.title}</h1>
+        <img src=  {props.image} alt={props.title}/>
+      </div>
+      <div className="body">
+        <p>Explanation: {props.explanation}</p>
+      </div>
+
+      <div className="less-Important"></div>
+      <h2> {props.date}</h2>
     </div>
   );
-}
+};
 
-export default App;
+export default AppContent;
 
 /* date: "2019-12-11"
 explanation: "What has this supernova left behind?  As little as 2,000 years ago, light from a massive stellar explosion in the Large Magellanic Cloud (LMC) first reached planet Earth. The LMC is a close galactic neighbor of our Milky Way Galaxy and the rampaging explosion front is now seen moving out - destroying or displacing ambient gas clouds while leaving behind relatively dense knots of gas and dust.  What remains is one of the largest supernova remnants in the LMC: N63A.  Many of the surviving dense knots have been themselves compressed and may further contract to form new stars.  Some of the resulting stars may then explode in a supernova, continuing the cycle.  Featured here is a combined image of N63A in the X-ray from the Chandra Space Telescope and in visible light by Hubble.  The prominent knot of gas and dust on the upper right -- informally dubbed the Firefox -- is very bright in visible light, while the larger supernova remnant shines most brightly in X-rays.  N63A spans over 25 light years and lies about 150,000 light years away toward the southern constellation of Dorado."
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import axios from 'axios';
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById("root"));
